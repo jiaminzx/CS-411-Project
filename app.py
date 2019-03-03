6cd7149783338553c08ea686211d70e1b847e754
Zhu, Jiamin <jiaminz2@illinois.edu>
2019-03-02 21:36:03 -0600

    :o

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+
+from werkzeug import generate_password_hash, check_password_hash
 app = Flask(__name__)
 application = app # our hosting requires application in passenger_wsgi
+
+
+mysql = MySQL()
  
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'pickles249_test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()	
+
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -10,9 +25,38 @@ def main():
 def m():
     return render_template('index.html')
 
-@app.route("/showSignUp")
-def showSignUp():  
-    return render_template('index2.html')
+
+
+@app.route('/showSignUp',methods=['POST'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # validate the received values
+        if _name and _email and _password:
+            
+            # All Good, let's call MySQL
+            
+           
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
 
 ###remove below if wsgi
 if __name__ == "__main__":
