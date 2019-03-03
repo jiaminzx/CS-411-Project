84061d0aed03edafddfdf1dc33b73adc03faa1b1
Zhu, Jiamin <jiaminz2@illinois.edu>
2019-03-02 20:39:10 -0600

    added some front end

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 application = app # our hosting requires application in passenger_wsgi
  
 @app.route("/")
-def hello():
-    return "This is Hello World!\n"
- 
+def main():
+    return render_template('index.html')
+
+@app.route("/main")
+def m():
+    return render_template('index.html')
+
+@app.route("/showSignUp")
+    
+def showSignUp():  
+    return render_template('index2.html')
+
+###remove below if wsgi
+if __name__ == "__main__":
+    app.run()
