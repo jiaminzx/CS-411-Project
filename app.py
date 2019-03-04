67fd36d5a82515f8eca9514a94f0c019fd054e1f
Zhu, Jiamin <jiaminz2@illinois.edu>
2019-03-03 21:38:39 -0600

    pls work

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def m():
     return render_template('index.html')
 @app.route("/showUsers")
 def showUsers():
-    mariadb_connection = mariadb.connect(user='rootuser', password='', database='employees')
+    mariadb_connection = mariadb.connect(user='rootuser', password='', database='cs411project')
     cursor = mariadb_connection.cursor()
     cursor.execute("SELECT name,email FROM employees WHERE name=%s", ('john',))
     return json.dumps({cursor[0]})
