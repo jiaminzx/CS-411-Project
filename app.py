94c1110f7d1ee00fbeff561eea98371c8ba76dfc
sheevm2 <sheevm2@illinois.edu>
2019-03-04 19:37:51 -0600

    checking app

--- app.py
+++ app.py
@@ -31,38 +31,5 @@ def showUsers():
 @app.route('/showSignUp')
 def signUp():
     return render_template('index2.html')
-    # try:
-    #     _name = request.form['inputName']
-    #     _email = request.form['inputEmail']
-    #     _password = request.form['inputPassword']
-
-    #     # validate the received values
-    #     if _name and _email and _password:
-            
-    #         # All Good, let's call MySQL
-            
-           
-    #         _hashed_password = generate_password_hash(_password)
-    #         cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-    #         data = cursor.fetchall()
-
-    #         if len(data) is 0:
-    #             conn.commit()
-    #             return json.dumps({'message':'User created successfully !'})
-    #         else:
-    #             return json.dumps({'error':str(data[0])})
-    #     else:
-    #         return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-    # except Exception as e:
-    #     return json.dumps({'error':str(e)})
-    # finally:
-    #     cursor.close() 
-    #     conn.close()
-
-
-
-###remove below if hosting on cpanel
 if __name__ == "__main__":
-    #app.run(debug=True)
     app.run(host='sp19-cs411-36.cs.illinois.edu', port=5001)
