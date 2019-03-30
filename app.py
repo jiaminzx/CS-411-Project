fa3241f5dc32b5a18e0c43c3773de7eeadb47990
jiaminzx <jiaminzx@gmail.com>
2019-03-30 13:31:47 -0500

    works on cpanel

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, json,jsonify, request
+#from flaskext.mysql import MySQL
+
 #import MySQL
 import mysql.connector as mariadb
 
@@ -18,7 +20,7 @@ def m():
 
 @app.route("/showUsers")
 def showUsers():
-    mariadb_connection = mariadb.connect(user='username', password='password', database='dbname')
+    mariadb_connection = mariadb.connect(user='pickles249_admin', password='Aelius999!', database='pickles249_test')
     cursor = mariadb_connection.cursor()
     cursor.execute("SELECT * FROM users")
     rows=cursor.fetchall()
@@ -31,38 +33,11 @@ def showUsers():
 @app.route('/showSignUp')
 def signUp():
     return render_template('index2.html')
-    # try:
-    #     _name = request.form['inputName']
-    #     _email = request.form['inputEmail']
-    #     _password = request.form['inputPassword']
-
-    #     # validate the received values
-    #     if _name and _email and _password:
-            
-    #         # All Good, let's call MySQL
-            
-           
-    #         _hashed_password = generate_password_hash(_password)
-    #         cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-    #         data = cursor.fetchall()
-
-    #         if len(data) is 0:
-    #             conn.commit()
-    #             return json.dumps({'message':'User created successfully !'})
-    #         else:
-    #             return json.dumps({'error':str(data[0])})
-    #     else:
-    #         return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-    # except Exception as e:
-    #     return json.dumps({'error':str(e)})
-    # finally:
-    #     cursor.close() 
-    #     conn.close()
+   
 
 
 
 ###remove below if hosting on cpanel
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host='sp19-cs411-36.cs.illinois.edu', port=5000)
+  #  app.run(host='sp19-cs411-36.cs.illinois.edu', port=5000)
