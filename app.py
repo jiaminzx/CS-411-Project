64fc1a6f3821662266fe77039726a503f07d8972
jiaminzx <35902352+jiaminzx@users.noreply.github.com>
2019-04-05 23:41:41 -0500

    latest working version should be this..

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, json,jsonify, request
-#import MySQL
 import mysql.connector as mariadb
-
-
 
 app = Flask(__name__)
 application = app # our hosting requires application in passenger_wsgi
@@ -18,11 +15,11 @@ def m():
 
 @app.route("/showUsers")
 def showUsers():
-    mariadb_connection = mariadb.connect(user='username', password='password', database='dbname')
+    mariadb_connection = mariadb.connect(user='pickles249_admin', password='csProject411!', database='pickles249_test')
     cursor = mariadb_connection.cursor()
     cursor.execute("SELECT * FROM users")
     rows=cursor.fetchall()
-    
+    print(rows)
     return jsonify(rows)
     # return render_template("showUser.html",rows=rows)
     mariadb_connection.close()
@@ -31,38 +28,7 @@ def showUsers():
 @app.route('/showSignUp')
 def signUp():
     return render_template('index2.html')
-    # try:
-    #     _name = request.form['inputName']
-    #     _email = request.form['inputEmail']
-    #     _password = request.form['inputPassword']
-
-    #     # validate the received values
-    #     if _name and _email and _password:
-            
-    #         # All Good, let's call MySQL
-            
-           
-    #         _hashed_password = generate_password_hash(_password)
-    #         cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-    #         data = cursor.fetchall()
-
-    #         if len(data) is 0:
-    #             conn.commit()
-    #             return json.dumps({'message':'User created successfully !'})
-    #         else:
-    #             return json.dumps({'error':str(data[0])})
-    #     else:
-    #         return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-    # except Exception as e:
-    #     return json.dumps({'error':str(e)})
-    # finally:
-    #     cursor.close() 
-    #     conn.close()
-
-
-
+    
 ###remove below if hosting on cpanel
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host='sp19-cs411-36.cs.illinois.edu', port=5000)
+    app.run(host='localhost')
