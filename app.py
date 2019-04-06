428402c68b60346f8e12bb4047d548c459887602
jiaminzx <35902352+jiaminzx@users.noreply.github.com>
2019-04-06 00:01:37 -0500

    works on cpanel

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, json,jsonify, request
+from flask import Flask, render_template, json, jsonify, request
 #import MySQL
 import mysql.connector as mariadb
 
+db = mariadb.connect(user='pickles249_admin', password='csProject411!', database='pickles249_test')
+cursor = db.cursor()
+
+
+#db.close() needs to be called to close connection
 
 
 app = Flask(__name__)
 application = app # our hosting requires application in passenger_wsgi
-	
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
 
 @app.route("/main")
 def m():
@@ -18,51 +23,43 @@ def m():
 
 @app.route("/showUsers")
 def showUsers():
-    mariadb_connection = mariadb.connect(user='username', password='password', database='dbname')
-    cursor = mariadb_connection.cursor()
     cursor.execute("SELECT * FROM users")
     rows=cursor.fetchall()
-    
+
     return jsonify(rows)
-    # return render_template("showUser.html",rows=rows)
-    mariadb_connection.close()
-    
+
 
 @app.route('/showSignUp')
 def signUp():
     return render_template('index2.html')
-    # try:
-    #     _name = request.form['inputName']
-    #     _email = request.form['inputEmail']
-    #     _password = request.form['inputPassword']
 
-    #     # validate the received values
-    #     if _name and _email and _password:
-            
-    #         # All Good, let's call MySQL
-            
-           
-    #         _hashed_password = generate_password_hash(_password)
-    #         cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-    #         data = cursor.fetchall()
+@app.route('/showSignUp/handle_data', methods=['POST'])
+def handle_data():
+    # print "HEEEEEEERE"
+    if request.method == 'POST':
+        projectpath = request.form['projectFilepath']
+        #print projectpath
 
-    #         if len(data) is 0:
-    #             conn.commit()
-    #             return json.dumps({'message':'User created successfully !'})
-    #         else:
-    #             return json.dumps({'error':str(data[0])})
-    #     else:
-    #         return json.dumps({'html':'<span>Enter the required fields</span>'})
+    return render_template('index2.html')
 
-    # except Exception as e:
-    #     return json.dumps({'error':str(e)})
-    # finally:
-    #     cursor.close() 
-    #     conn.close()
+  
+# @app.route('/addU')
+@app.route('/showSignUp/adduser', methods=['POST'])
+def adduser():
+    # print "Entered"
+    if request.method == 'POST':
+        try:
+            name = request.form['inputName']
+            password = request.form['inputPassword']
+            #print name, password
+            cursor.execute("INSERT INTO users (name, email) VALUES (%s,%s)",(name, password))
+            db.commit()
+            # print "Registered"
+        except Exception as e:
+          return(str(e))
+    return render_template('index2.html')
 
+# #comment out when hosting on cpanel
 
-
-###remove below if hosting on cpanel
-if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host='sp19-cs411-36.cs.illinois.edu', port=5000)
+# if __name__ == "__main__":
+#     app.run(host='sp19-cs411-36.cs.illinois.edu', port=8081)
