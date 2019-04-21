37d42c5d6a380b3ee94d5942a73a46da112e59a1
Carson Zhang <cxzhang4@illinois.edu>
2019-04-21 14:55:05 -0500

    Linked sign-in button to basic profile display page (functionality not tested yet)

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import Flask, render_template, json, jsonify, request
 import mysql.connector as mariadb
 
 #Use this line for cPanel
-# db = mariadb.connect(user='pickles249_admin', password='csProject411!', database='pickles249_test')
+db = mariadb.connect(user='pickles249_admin', password='csProject411!', database='pickles249_test')
 #Use this line for VM
 # db = mariadb.connect(user='root', password='password', database='cs411project')
-# cursor = db.cursor()
+cursor = db.cursor()
 
 
 #db.close() needs to be called to close connection
@@ -67,20 +67,22 @@ def adduser():
     return render_template('signup.html')
 
 @app.route('/showSignIn')
-def showSignIn():
+def signIn():
     return render_template('signIn.html')
 
-@app.route('/showSignIn', methods = ['GET'])
+@app.route('/get_profile', methods = ['GET'])
 def get_profile():
     if request.method == 'GET':
         try:
             password = request.form['inputPassword']
             email = request.form['inputEmail']
             cursor.execute('SELECT * FROM users WHERE email="%s"' % (email))
-            rows=cursor.fetchall()
-            assert len(rows) == 1, "Multiple profiles with same email"
+            row=cursor.fetchall()
+            assert len(row) == 1, "Multiple profiles with same email"
         except Exception as e:
           return(str(e))
+
+        return render_template('profile.html', data = row)
 
 @app.route('/showModify', methods=['POST'])
 def moduser():
@@ -152,6 +154,6 @@ def showWomen():
     return render_template('showWomen.html', data=rows)
 
 # #comment out when hosting on cpanel
-if __name__ == "__main__":
+# if __name__ == "__main__":
 # #     app.run(host='sp19-cs411-36.cs.illinois.edu', port=8083)
-    app.run()
+    # app.run()
