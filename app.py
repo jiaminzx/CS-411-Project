cbb4262695e7534ec826d47e79c5495308079356
Pratheek-RM <prmuskula@gmail.com>
2019-05-03 23:42:35 -0500

    added messages page for sheev

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import Flask, render_template, json, jsonify, request
 import mysql.connector as mariadb
 
 #Use this line for cPanel
-db = mariadb.connect(user='pickles249_admin', password='csProject411!', database='pickles249_test')
+# db = mariadb.connect(user='pickles249_admin', password='csProject411!', database='pickles249_test')
 #Use this line for VM
-# db = mariadb.connect(user='root', password='password', database='cs411project')
+db = mariadb.connect(user='root', password='password', database='cs411project')
 cursor = db.cursor()
 
 
@@ -32,6 +32,10 @@ def modify():
 @app.route('/showDelete')
 def delete():
     return render_template('delete.html')
+
+@app.route('/showMessage')
+def messages():
+    return render_template('messages.html')
 
 @app.route('/showSignUp/handle_data', methods=['POST'])
 def handle_data():
@@ -137,7 +141,6 @@ def showWomen():
     return render_template('showWomen.html', data=rows)
 
 # #comment out when hosting on cpanel
-# if __name__ == "__main__":
-# #     app.run(host='sp19-cs411-36.cs.illinois.edu', port=8083)
-#     app.run()
-
+if __name__ == "__main__":
+    app.run(host='sp19-cs411-36.cs.illinois.edu', port=8082)
+    # app.run()
