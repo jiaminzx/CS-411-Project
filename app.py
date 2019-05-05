4b88febf21284b5c9de995eff6a1a2f6ac6ac51c
Carson Zhang <cxzhang4@illinois.edu>
2019-05-04 23:22:36 -0500

    adjusted debuggin

--- app.py
+++ app.py
@@ -317,8 +317,9 @@ def show_user_queue():
                 rows=cursor.fetchall()
 
                 # insert into yeses_tbl
-                decision = request.form["value"]
-                print(decision)
+                # decision = request.form["value"]
+                decision = request.data
+                # print(decision)
                 # if decision == "yes":
                 cursor.execute("INSERT INTO yeses_tbl (prospecting_id, viewed__id) VALUES ({},{})".format(str(userID), rows[0]))
             except mysql.connector.Error as error:
@@ -331,7 +332,7 @@ def show_user_queue():
                 rows=cursor.fetchall()
 
                 # insert into yeses_tbl
-                decision = request.form["value"]
+                # decision = request.form["value"]
                 print("able to access value in form")
                 decision = request.data
                 # if decision == "yes":
