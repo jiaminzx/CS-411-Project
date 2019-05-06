97e07a4818a5b8507e7165a34006e7d712b8642e
Modi <sheevm2@sp19-cs411-36.cs.illinois.edu>
2019-05-05 19:49:31 -0500

    fjei

--- app.py
+++ app.py
@@ -452,5 +452,5 @@ def load_user(userid):
 
 # #comment out when hosting on cpanel
 if __name__ == "__main__":
-    app.run(host='sp19-cs411-36.cs.illinois.edu', port=8081)
+    app.run(host='sp19-cs411-36.cs.illinois.edu', port=8040)
     # app.run()
